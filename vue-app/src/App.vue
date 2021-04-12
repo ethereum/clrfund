2caba5343408630d208ba348cd81b13b354b63eb
Sam Richards <sbrichards@gmail.com>
2021-04-12 16:58:18 -0700

    Fallback $route to empty string

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -65,7 +65,7 @@ export default class App extends Vue {
 
   get isSidebarCollapsed(): boolean {
     const routes = ['landing', 'projectAdded', 'join', 'joinStep']
-    return routes.includes(this.$route.name)
+    return routes.includes(this.$route.name || '')
   }
 }
 </script>
